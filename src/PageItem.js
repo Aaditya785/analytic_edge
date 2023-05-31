875133ffb7a76f148f0c637fa28e695e990070a7
Aaditya785 <aadityasandil5@gmail.com>
2023-05-31 22:30:44 +0530

    pagination_complete

--- src/PageItem.js
+++ src/PageItem.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
-// import './PageItem.css'; // Import the CSS file
+
 
 function Items({ currentItems }) {
   return (
