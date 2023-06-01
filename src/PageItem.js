9fb1ccc69e04f1bcc7f5fe1349a6dc42a535847d
Aaditya785 <aadityasandil5@gmail.com>
2023-06-01 20:43:33 +0530

    pagination_2

--- src/PageItem.js
+++ src/PageItem.js
@@ -8,26 +8,37 @@ function Items({ currentItems }) {
       {currentItems &&
         currentItems.map((item) => (
           <div className="item-container" key={item.id}>
-            <h3>{item.title}</h3>
+            <h3>{item.id}</h3>
           </div>
         ))}
     </>
   );
 }
 
-function PageItem({ itemsPerPage }) {
+function PageItem({ itemsPerPage }) {// 5 item each page
   const [itemOffset, setItemOffset] = useState(0);
   const [currentItems, setCurrentItems] = useState([]);
   const [totalItems, setTotalItems] = useState(0);
+
+  const forLength = async() => {
+    const response = await fetch(
+      `http://jsonplaceholder.typicode.com/posts`
+    );
+    const data = await response.json();
+    console.log("Result:--:", data.length);
+    setTotalItems(data.length);
+  }
+  forLength();
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch(
         `http://jsonplaceholder.typicode.com/posts?_start=${itemOffset}&_limit=${itemsPerPage}`
       );
+
       const data = await response.json();
       setCurrentItems(data);
-      setTotalItems(itemsPerPage);
+      // setTotalItems(itemLength.length);
     };
 
     fetchData();
